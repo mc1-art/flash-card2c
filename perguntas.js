13d28bc3a982576fbd6707aec7b00d6a23329a48
mc1-art <vinicius.silveira.marcos@escola.pr.gov.br>
2025-05-12 14:46:04 +0000

    ok

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript ?',
+     'Linguagem de Programação'
+)
+
+
+
+
+
+criaCartao(
+    'Inglês',
+    'Como se escreve dia do pagamento?',
+    'Pay Day'
+)
+
+criaCartao(
+    'matematica',
+    'Qual o grafico da função quadratica? ',
+    'Parabola'
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
